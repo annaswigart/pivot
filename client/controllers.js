83b68723c9d04a1e4d23b0b431620dac4c926bc6
Cameron Reed <camireed@gmail.com>
2015-04-24 20:01:05 -0700

    dummy text for work activities and work environment

--- client/controllers.js
+++ client/controllers.js
@@ -195,6 +195,20 @@ angular.module('reflectivePath').controller('ResultsController', ['$scope', '$me
         return _.contains($scope.pinnedCareers, career);
     }
 
+    // placeholder text
+    $scope.placeholderText = [
+        {text: "Vivendum incorrupte nam cu, eu eam alii dolor scribentur, an everti option principes eum."},
+        {text: "Qui cu epicurei accusamus. Eu dicit partem erroribus per, ei diam labitur volumus vel, et mel quis nominavi."},
+        {text: "Ex mel tamquam recusabo. His aliquip accusata an, stet consul ne sit, mucius possim pri in."},
+        {text: "Recteque disputando signiferumque no vis. In rebum numquam pri."},
+        {text: "Sed primis adipiscing eu, te pro graecis nominavi reprehendunt. Aeterno integre fierent no his, at est propriae copiosae. "},
+        {text: "Ne posse tractatos definiebas sea, ei exerci putent mea, ne deserunt."},
+        {text: "At pri movet audire feugiat, vix eu alia urbanitas. Cu his atqui facilis facilisi, eos at velit sadipscing."},
+        {text: "Malorum offendit vis ei, purto aperiri neglegentur ex mel, omnis numquam mei et."},
+        {text: "Vim dicunt nominati te, amet periculis vim ei. Forensibus reprimique ne sea."},
+        {text: "Per vidisse perfecto aliquando id. Purto timeam ius at, sit modo dico maiorum et. Eos at atomorum deseruisse."}
+    ];
+
 }]);
 
 // Start from filter
@@ -246,6 +260,19 @@ function($scope, $meteor, $stateParams, $window){
         return _.contains($scope.pinnedCareers, career);
     }
 
+    // placeholder text
+    $scope.placeholderText = [
+        {text: "Vivendum incorrupte nam cu, eu eam alii dolor scribentur, an everti option principes eum."},
+        {text: "Qui cu epicurei accusamus. Eu dicit partem erroribus per, ei diam labitur volumus vel, et mel quis nominavi."},
+        {text: "Ex mel tamquam recusabo. His aliquip accusata an, stet consul ne sit, mucius possim pri in."},
+        {text: "Recteque disputando signiferumque no vis. In rebum numquam pri."},
+        {text: "Sed primis adipiscing eu, te pro graecis nominavi reprehendunt. Aeterno integre fierent no his, at est propriae copiosae. "},
+        {text: "Ne posse tractatos definiebas sea, ei exerci putent mea, ne deserunt."},
+        {text: "At pri movet audire feugiat, vix eu alia urbanitas. Cu his atqui facilis facilisi, eos at velit sadipscing."},
+        {text: "Malorum offendit vis ei, purto aperiri neglegentur ex mel, omnis numquam mei et."},
+        {text: "Vim dicunt nominati te, amet periculis vim ei. Forensibus reprimique ne sea."},
+        {text: "Per vidisse perfecto aliquando id. Purto timeam ius at, sit modo dico maiorum et. Eos at atomorum deseruisse."}
+    ];
    
 
 
