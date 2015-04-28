e423ff2a1acda09f18a8bbf45db2fec2d8c81276
Cameron Reed <camireed@gmail.com>
2015-04-27 18:22:46 -0700

    cleanup SidebarController

--- client/controllers.js
+++ client/controllers.js
@@ -452,66 +452,9 @@ angular.module('reflectivePath').controller('SidebarController', ['$scope', '$me
 function($scope, $meteor, $stateParams, $window){
 
 
-    // // init pinned careers in local storage
-    // if($window.localStorage.getItem("pinnedCareers") === null){
-    //     $window.localStorage.pinnedCareers = '[]';
-    // }
-    
-    // // parse string from local storage
-    // $scope.pinnedCareers = JSON.parse($window.localStorage.pinnedCareers);
+    // apply checkbox style if checked
 
-    // // toggle pinned career (remove / add)
-    // $scope.togglePinnedCareer = function(career){
-    //     if (_.contains($scope.pinnedCareers, career)) {
-    //         $scope.pinnedCareers = _.without($scope.pinnedCareers, career);
-    //         $window.localStorage.pinnedCareers = JSON.stringify($scope.pinnedCareers);
-    //     } else {
-    //         $scope.pinnedCareers.push(career);
-    //         $window.localStorage.pinnedCareers = JSON.stringify($scope.pinnedCareers);
-    //     }
-    // }
-
-    // // apply class to pinned careers
-    // $scope.isPinned = function(career) {
-    //     return _.contains($scope.pinnedCareers, career);
-    // }
-
-    // // init viewed careers in local storage
-    // if($window.localStorage.getItem("viewedCareers") === null){
-    //     $window.localStorage.viewedCareers = '[]';
-    // }
-
-    // // parse string from local storage
-    // $scope.viewedCareers = JSON.parse($window.localStorage.viewedCareers);
-
-    // // view career
-    // $scope.viewCareer = function(careerName, careerId) {
-
-    //     var viewedCareer = {name: careerName, id: careerId};
-
-    //     // update array of viewed careers and only keep 3
-    //     $scope.viewedCareers.unshift(viewedCareer);
-        
-    //     // temp var for the for loop
-    //     var uniqIds = [];
-    //     var listCopy = [];   
-
-    //     // iterate through viewdCareers
-    //     for (var i = 0; i < $scope.viewedCareers.length; i++) {
-    //         var thisId = $scope.viewedCareers[i].id;
-    //         if (!_.contains(uniqIds, thisId)) {
-    //             // track ids encountered
-    //             uniqIds.push(thisId);
-    //             // add uniq object to listCopy
-    //             listCopy.push($scope.viewedCareers[i]);
-    //         }
-    //     }
-
-    //     // update with deduped list, keep only 3
-    //     $scope.viewedCareers = listCopy.slice(0,3);
-
-    //     $window.localStorage.viewedCareers = JSON.stringify($scope.viewedCareers);
-    // }
+    // apply inactive style when less than 2 checked
 
 }]);
 
